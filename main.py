b9618ff4f1ae7f52e799d2d3debb4d4083a64831
Luiz Medina <luizmedina87@gmail.com>
2026-06-08 15:13:40 -0400

    add verbose

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("API key not found")
-
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    if response.usage_metadata:
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    response = generate_content(client, messages)    
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+
+    if not response.usage_metadata:
+        raise RuntimeError("Failed API request")
+    elif args.verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise RuntimeError("Failed API request")
+        
     print(response.text)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=messages
+    )
+    return response
 
 if __name__ == "__main__":
     main()
