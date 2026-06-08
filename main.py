57f42349a76b631f05569b59e054a31b2482630d
Luiz Medina <luizmedina87@gmail.com>
2026-06-08 11:24:49 -0400

    add token usage printing

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise RuntimeError("API key not found")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("API key not found")
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash-lite",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    if response.usage_metadata:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        raise RuntimeError("Failed API request")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
