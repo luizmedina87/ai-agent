097b722ac968cba62d4e1076968f4438e04d3775
Luiz Medina <luizmedina87@gmail.com>
2026-06-09 11:44:55 -0400

    read dir contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,10 +9,18 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
 		if not valid_target_dir:
 			return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
 		
-		if not os.path.isdir(directory):
+		if not os.path.isdir(target_directory):
 			return(f'Error: "{directory}" is not a directory')
+		
+		directory_items: list[str] = []
+		for item in os.listdir(target_directory):
+			item_abs = os.path.join(target_directory, item)
+			item_size = os.path.getsize(item_abs)
+			isdir_item = os.path.isdir(item_abs)
 
-		return f'Success: "{directory}" is within the working directory'
+			directory_items.append(f"- {item_abs}: file_size={item_size} bytes, is_dir={isdir_item}")
+
+		return "\n".join(directory_items)
 	
 	except Exception as e:
 		return f"Error: {e}"
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,10 @@
 from functions.get_files_info import get_files_info
 
+print(f"Result for current directory:")
 print(get_files_info("calculator", "."))
+print(f"Result for current directory:")
+print(get_files_info("calculator", "pkg"))
+print(f"Result for current directory:")
 print(get_files_info("calculator", "/bin"))
+print(f"Result for current directory:")
 print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
