b9c0d62085d934aab244262b1e81773993297a37
Luiz Medina <luizmedina87@gmail.com>
2026-06-11 13:27:54 -0400

    complete feature to run python file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,5 @@
 import os
-
+import subprocess
 
 def run_python_file(
     working_directory: str, file_path: str, args: list[str] | None = None
@@ -15,12 +15,27 @@ def run_python_file(
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
         if not target_is_file:
-            return f'Error: Cannot write to "{file_path}" as it is a directory'
+            return f'Error: "{file_path}" does not exist or is not a regular file'
         
         if not target_is_python:
             return f'Error: "{file_path}" is not a Python file'
 
-        return "All good"
+        command = ["python", target_file_path]
+        if args:
+            command.extend(args)
+
+        result: subprocess.CompletedProcess = subprocess.run(command, capture_output=True, text=True, timeout=30)
+
+        result_description = f"Process exited with code {result.returncode}."
+
+        if result.stdout:
+            result_description += f" STDOUT: {result.stdout}."
+        elif result.stderr:
+            result_description += f" STDERR: {result.stderr}."
+        else:
+            result_description += f" No output produced."
+
+        return result_description
     
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error: executing Python file: {e}"
--- test_run_python_file.py
+++ test_run_python_file.py
@@ -1,20 +1,19 @@
-# test_run_python_file.py
 from functions.run_python_file import run_python_file
 
-print("--- Test 1: Usage Instructions ---")
+print("--- Test 1: should print the calculator's usage instructions ---")
 print(run_python_file("calculator", "main.py"))
 
-print("\n--- Test 2: Calculator with input '3 + 5' ---")
+print("\n--- Test 2: should run the calculator... which gives a kinda nasty rendered result ---")
 print(run_python_file("calculator", "main.py", ["3 + 5"]))
 
-print("\n--- Test 3: Running calculator tests ---")
+print("\n--- Test 3: should run the calculator's tests successfully ---")
 print(run_python_file("calculator", "tests.py"))
 
-print("\n--- Test 4: Attempting to access ../main.py ---")
+print("\n--- Test 4: Attempting to access ../main.py (this should return an error) ---")
 print(run_python_file("calculator", "../main.py"))
 
-print("\n--- Test 5: Running non-existent file ---")
+print("\n--- Test 5: Running non-existent file (this should return an error) ---")
 print(run_python_file("calculator", "nonexistent.py"))
 
-print("\n--- Test 6: Running text file ---")
+print("\n--- Test 6: Running text file (this should return an error) ---")
 print(run_python_file("calculator", "lorem.txt"))
